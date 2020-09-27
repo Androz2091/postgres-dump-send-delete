c463aa6d7a7e3ee8ff47a9a7abeb6b8b6d9a48e8
Androz2091 <androz2091@gmail.com>
2020-09-27 21:01:02 +0200

    :tada: Init repository

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# postgres-dump-send-delete
- PostgreSQL db system. Dump. Send. Delete.
+# Postgres Dump Send Delete
+
+This script makes a dump of your PG database, then converts it to a tar file and sends it to a remote server. Once the transfer is done, the script will delete the dump from your server.
+
+## Installation
+
+* Install Curl. (`apt-get install curl`)
+* Download the script. (`curl -O https://raw.githubusercontent.com/Androz2091/postgres-dump-send-delete/master/backup.sh`)
+* Configure the script using a text editor.
+* Setup ssh keys so `rsync` can be used to transfer files without typing the password.
+* Setup Cron so the script is run every 6 hours. (`0 */6 * * * /bin/bash /path/to/backup.sh`)
+* You're done.
---
+++ backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TIMESTAMP=`date +%F-%H%M`
+
+PG_PATH="/usr/lib/postgresql/12/bin/pg_dump"
+PG_DATABASE="postgres" # Name of the database to backup
+APP_NAME="manageinvite" # Name of your application
+
+BACKUPS_DIR="/home/androz/backups/$APP_NAME" # Backups directory
+BACKUP_NAME="$APP_NAME-$TIMESTAMP"
+
+REMOTE_HOST_USERNAME="androz" # User used for scp
+REMOTE_HOST_ADDRESS="XXX.XXX.XXX.X" # The IP address of the remote server
+REMOTE_HOST_PATH="/home/androz/backups" # The path where the backups need to be stored on the remote server
+
+# Make the backups directory if it doesn't exist
+mkdir -p $BACKUPS_DIR
+
+# Dump the Postgres database
+$PG_PATH -F p -f $BACKUPS_DIR/$BACKUP_NAME.sql -U postgres $PG_DATABASE
+
+# Make a tar file of the dump
+tar -zcvf $BACKUPS_DIR/$BACKUP_NAME.tar.gz $BACKUPS_DIR/$BACKUP_NAME.sql
+
+# Delete the dump
+rm -rf $BACKUPS_DIR/$BACKUP_NAME.sql
+
+# Synchronize the backups folder with the remote server (send the missing file)
+rsync -avz --delete -e ssh $BACKUPS_DIR $REMOTE_HOST_USERNAME@$REMOTE_HOST_ADDRESS:$REMOTE_HOST_PATH
+
+# Then delete the tar file
+rm -rf $BACKUP_NAME.tar.gz
